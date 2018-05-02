bf17eaddb33a005adcd7065541c14f3c89d0b3df
YovoMitev <iovo.mitev@abv.bg>
2018-05-02 13:27:21 +0300

    Added useless About page
    Added useless About page

---
+++ client/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const About = (props) => (
+  <div>
+    <h1>About Page</h1>
+    <hr></hr>
+    <h2>Contact: +359 887 121 314</h2>
+
+  </div>
+)
+
+export default About
+
--- client/src/components/common/Routes.js
+++ client/src/components/common/Routes.js
@@ -4,11 +4,13 @@ import HomePage from '../todo/HomePage'
 import NotFoundPage from '../NotFoundPage'
 import CreateTodoPage from '../todo/CreateTodoPage'
 import DetailsTodoPage from '../todo/DetailsTodoPage'
+import About from '../About'
 
 const Routes = () => (
   <Switch>
     <Route exact path='/'  component={HomePage}/>
     <Route  path='/create'  component={CreateTodoPage}/>
+    <Route  path='/about'  component={About}/>
     <Route path='/details/:id/' component={DetailsTodoPage} />
     <Route component={NotFoundPage} />
   </Switch>
